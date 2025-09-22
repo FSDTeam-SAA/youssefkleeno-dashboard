e539b06bb65c5448ee814b2c87da193918f68ef6
mehedishishir0 <mehedihasanshishir@gmail.com>
2025-09-22 10:27:39 +0600

    add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
-			 btnPrimary: '#797068',
+			 btnPrimary: '#499FC0',
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
